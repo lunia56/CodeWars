4e9076dd70061919ea0b5db1c1fe721df953ce01
lunia56 <101800801+lunia56@users.noreply.github.com>
2022-06-24 09:34:45 +0300

    commit-2

--- codeWars.js
+++ codeWars.js
@@ -16,4 +16,21 @@ function billboard(name, price = 30) {
         totalPrice += price;
     }
     return totalPrice
-} 
+}
+
+
+
+// Your task is to make two functions 
+// ( max and min, or maximum and minimum, etc., depending on the language ) 
+// that receive a list of integers as input, 
+// and return the largest and lowest number in that list, respectively.
+
+let min = function (list) {
+    list.sort((a, b) => a - b)
+    return list[0];
+}
+
+let max = function (list) {
+    list.sort((a, b) => b - a)
+    return list[0];
+}
